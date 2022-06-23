a414ca46e713a377a0090432a7fd7591c9ff36e9
gabi-fernandes <gabi.fernandes@ibm.com>
2022-06-23 11:30:30 -0300

    ok

--- src/App.js
+++ src/App.js
@@ -2,21 +2,27 @@ import './App.css';
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
+import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
+import ChangingProgressProvider from './ChangingProgressProvider';
+
 function App() {
 	const [convidado, setConvidado] = useState();
 	const [confirmado, setConfirmado] = useState(false);
+	const [loading, setLoading] = useState(true);
+	const [no, setNo] = useState(false);
+
 	useEffect(() => {
 		let url = window.location.pathname.split('/')[1];
-		console.log(url);
-
 		sendData();
-
 		async function sendData() {
-			let usuario = await axios.post(`${process.env.REACT_APP_URL4}`, [
-				`${window.location.pathname.split('/')[1]}`,
-			]);
-			setConvidado(usuario.data[0]);
-			console.log(usuario);
+			if (window.location.pathname !== '/') {
+				let usuario = await axios.post(`${process.env.REACT_APP_URL4}`, [
+					`${window.location.pathname.split('/')[1]}`,
+				]);
+				setConvidado(usuario.data[0]);
+			} else {
+				setNo(true);
+			}
 		}
 	}, []);
 
@@ -29,26 +35,51 @@ function App() {
 	}
 	return (
 		<div className="App">
-			{window.location.pathname !== '' && convidado ? (
-				<>
-					<p id="white">CONVIDADO: {convidado.nome}</p>
-
-					<p id="white">
-						{convidado.ENTROU == 'S' ? (
-							<p> Convidado já entrou.</p>
-						) : (
-							<div onClick={entrou}>
-								{confirmado ? (
-									<p id="green">CONVIDADO CONFIRMADO!</p>
-								) : (
-									<p id="black">CONFIRMAR</p>
-								)}
-							</div>
-						)}
-					</p>
-				</>
+			{no ? (
+				<p id="white">NENHUM CONVIDADO</p>
 			) : (
-				<p id="white">BUSCANDO CONVIDADO...</p>
+				<>
+					{convidado ? (
+						<>
+							<p id="white">CONVIDADO: {convidado.nome}</p>
+
+							<p id="white">
+								{convidado.ENTROU == 'S' ? (
+									<p> Convidado já entrou.</p>
+								) : (
+									<div onClick={entrou}>
+										{confirmado ? (
+											<p id="green">CONVIDADO CONFIRMADO!</p>
+										) : (
+											<p id="black">CONFIRMAR</p>
+										)}
+									</div>
+								)}
+							</p>
+						</>
+					) : (
+						<>
+							<p id="white">BUSCANDO CONVIDADO...</p>
+						</>
+					)}
+				</>
+			)}
+
+			{loading ? (
+				<div style={{ width: 50, height: 50 }}>
+					<ChangingProgressProvider values={[100, 0]}>
+						{(percentage) => (
+							<CircularProgressbar
+								styles={buildStyles({
+									rotation: (1 - percentage / 100) / 2,
+								})}
+								value={percentage}
+							/>
+						)}
+					</ChangingProgressProvider>
+				</div>
+			) : (
+				''
 			)}
 		</div>
 	);
